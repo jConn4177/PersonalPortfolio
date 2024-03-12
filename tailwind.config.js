b3ee5096c51d1a63d8d2ea0c18ef561060017799
Jason <jconn99801@gmail.com>
2024-03-12 09:19:46 -0800

    fixed tailwind issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
       colors: {
-        blue: "#2cbce9",
-        red: "#dc4492",
-        yellow: "#fdcc49",
+        "deep-blue": "#010026",
+        blue: "#2CBCE9",
+        red: "#DC4492",
+        yellow: "#FDCC49",
         grey: "#ededed",
-        "deep-blue": "#010026:",
+        "dark-grey": "#757575",
         "opaque-black": "rgba(0,0,0,0.35)",
       },
       backgroundImage: (theme) => ({
         "gradient-rainbow":
-          "linear-gradient(81.66deg, #00b5ee 7.21%, #ff45a4 45.05%, #ffb), #ffba00 78.07%)",
+          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+
         "gradient-rainblue":
-          "liner-gradient(90deg, #24cbff 14.53%, #fc59ff 69.36%, #ffbd0c 117.73",
+          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
       }),
       fontFamily: {
         playfair: ["Playfair Display", "serif"],
@@ -31,8 +33,11 @@ export default {
     },
     screens: {
       xs: "480px",
+      ss: "620px",
       sm: "768px",
       md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
