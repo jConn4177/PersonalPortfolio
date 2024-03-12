2a480f703c0963db6a6666a55dbd9b0c1045a61e
Jason <jconn99801@gmail.com>
2024-03-11 20:37:07 -0800

    configured theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2cbce9",
+        red: "#dc4492",
+        yellow: "#fdcc49",
+        grey: "#ededed",
+        "deep-blue": "#010026:",
+        "opaque-black": "rgba(0,0,0,0.35)",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow":
+          "linear-gradient(81.66deg, #00b5ee 7.21%, #ff45a4 45.05%, #ffb), #ffba00 78.07%)",
+        "gradient-rainblue":
+          "liner-gradient(90deg, #24cbff 14.53%, #fc59ff 69.36%, #ffbd0c 117.73",
+      }),
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      content: {
+        brush: "url('./assets/brush.png')",
+        person1: "url('./assets/person-1.png')",
+        person2: "url('./assets/person-2.png')",
+        person3: "url('./assets/person-3.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
-}
-
+};
